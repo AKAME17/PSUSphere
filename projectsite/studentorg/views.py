3475f8d6a9f67b68ca29fb475dd6508c78a68c96
AKAME17 <nelyzagalia21@gmail.com>
2025-04-03 22:17:48 +0800

    OrgMember successful, List, Add, Edit, Delete

--- projectsite/studentorg/views.py
+++ projectsite/studentorg/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from studentorg.models import Organization
-from studentorg.forms import OrganizationForm
+from studentorg.models import Organization, OrgMember
+from studentorg.forms import OrganizationForm, OrgMemberForm
 from django.urls import reverse_lazy
 
 class HomePageView(ListView):
@@ -32,4 +32,39 @@ class OrganizationDeleteView(DeleteView):
     model = Organization
     template_name = 'org_del.html'
     success_url = reverse_lazy('organization-list')
+
+#ORGMEMBER
+class OrgMemberListView(ListView):
+    model = OrgMember
+    context_object_name = 'orgmember'
+    template_name = 'orgmember_list.html'
+    paginate_by = 5
+    
+    def get_queryset(self, *args, **kwargs):
+        qs = super().get_queryset(*args, **kwargs)
+        query = self.request.GET.get('q')
+        if query:
+            qs = qs.filter(
+                Q(student__lastname__icontains=query) |  # Look for student's lastname
+                Q(student__firstname__icontains=query) |  # Look for student's firstname
+                Q(organization__name__icontains=query)  # Look for organization's name
+            )
+        return qs
+class OrgMemberCreateView(CreateView):
+    model = OrgMember
+    form_class = OrgMemberForm
+    template_name = 'orgmember_add.html'
+    success_url = reverse_lazy('orgmember-list')
+    
+
+class OrgMemberUpdateView(UpdateView):
+    model = OrgMember
+    form_class = OrgMemberForm
+    template_name = 'orgmember_edit.html'
+    success_url = reverse_lazy('orgmember-list')
+
+class OrgMemberDeleteView(DeleteView):
+    model = OrgMember
+    template_name = 'orgmember_delete.html'
+    success_url = reverse_lazy('orgmember-list')
 # Create your views here.
